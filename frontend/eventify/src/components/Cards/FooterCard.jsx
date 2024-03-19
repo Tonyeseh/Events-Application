d75d781be175971356df4e6609775040948e9b75
esehtony <esehtony123@gmail.com>
2024-03-19 14:10:52 +0100

    FooterCard, EventCard added

---
+++ frontend/eventify/src/components/Cards/FooterCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FooterCard = ({header, listItems}) => {
+    return (
+        <div>
+            <h2 className="text-lg font-bold text-white mb-4">{header}</h2>
+            <ul>
+                {listItems && listItems.map(item => {
+                    return <li className="p-0.5">{item}</li>
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default FooterCard

52f328d08e131cc4f90cf8afc413471f93652184
esehtony <esehtony123@gmail.com>
2024-03-19 14:15:08 +0100

    updated App.js

--- frontend/eventify/src/App.js
+++ frontend/eventify/src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import Header from './components/Header';
 import Hero from './components/Hero'
+import CategorySection from "./components/CategorySection";
+import PopularInLocation from "./components/PopularInLocation";
+import BestOnlineSection from "./components/BestOnlineSection";
+import TrendingSection from "./components/TrendingSection";
+import Footer from "./components/Footer";
+import NewsLetterSection from "./components/NewLetterSection";
 
 function App() {
   return (
     <div>
         <Header />
         <Hero />
+        <CategorySection />
+        <PopularInLocation />
+        <BestOnlineSection />
+        <TrendingSection />
+        <NewsLetterSection />
+        <Footer />
     </div>
   );
 }

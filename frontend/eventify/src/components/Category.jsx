6327f2439a6391ca58725fced3e754f5dcd620d0
esehtony <esehtony123@gmail.com>
2024-03-19 01:14:55 +0100

    frontend initialization

---
+++ frontend/eventify/src/components/Category.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Category = () => {
+    return (
+        <div>
+            im
+        </div>
+    )
+}
+
+export default Category
---
+++ frontend/eventify/src/components/Header.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import logo from '../img/logo.png'
+
+const Header = () => {
+    const [header, setHeader] = useState(false)
+
+    const headerHandler = () => {
+        setHeader(!header)
+    }
+
+    return (
+        <div className="flex justify-between items-center h-12 bg-[#2B293D] text-white px-20 py-4 m-auto">
+            <img src={logo} alt="Logo" className="w-40" />
+            <ul className="hidden md:flex">
+                <li className="py-3 px-6 border-b border-gray-500-2 border-[#ffe047]">Home</li>
+                <li className="py-3 px-6">Events</li>
+                <li className="py-3 px-6">About</li>
+                <li className="py-3 px-6">Contact</li>
+            </ul>
+            <ul className="hidden md:flex items-center">
+                <li className="p-3">Create Event</li>
+                <li className="p-3 text-sm flex flex-col items-center"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 0 1 0 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 0 1 0-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375Z" />
+</svg>
+Tickets</li>
+                <li className="p-3 text-sm flex flex-col items-center"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+</svg>
+Interested</li>
+                <li className="p-3 text-sm flex flex-col items-center"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+</svg>
+                Profile</li>
+            </ul>
+            <div onClick={headerHandler} className="block md:hidden">
+                {!header ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+</svg> : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+  <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+</svg>}
+            
+            </div>
+            <div className={header ? 'fixed left-0 top-0 w-[60%] border-r border-r-white h-full p-3 bg-[#2B293D] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+            <img src={logo} alt="Logo" className="w-40" />
+                <ul className="p-3">
+                    <li className="py-3 px-6 border-b border-gray-500">Home</li>
+                    <li className="py-3 px-6 border-b border-gray-500">Events</li>
+                    <li className="py-3 px-6 border-b border-gray-500">About</li>
+                    <li className="py-3 px-6 border-b border-gray-500">Contact</li>
+                
+                    <li className="py-3 px-6 border-b border-gray-500">Create Event</li>
+                    <li className="py-3 px-6  flex items-center border-b border-gray-500"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-6 pr-2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 0 1 0 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 0 1 0-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375Z" />
+</svg>
+Tickets</li>
+                    <li className="py-3 px-6 flex items-center border-b border-gray-500"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-6 pr-2">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+</svg>
+Interested</li>
+                <li className="py-3 px-6 flex items-center border-b border-gray-500"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-6 pr-2">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+</svg>
+                Profile</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header;

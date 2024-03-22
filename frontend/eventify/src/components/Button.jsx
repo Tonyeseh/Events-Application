fa1977bb1933554b71920dab15ae67aa578b8d40
esehtony <esehtony123@gmail.com>
2024-03-22 08:41:30 +0100

    added button component

---
+++ frontend/eventify/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = ({ action, type }) => {
+    return (
+        <button className="w-full py-3 px-10 rounded-md bg-[#2B293D] text-white text-base font-bold" type={type}>
+            {action}
+        </button>
+    )
+}
+
+export default Button;

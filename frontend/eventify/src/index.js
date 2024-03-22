bfadea4b5209f03a98989309af3e34e703759697
esehtony <esehtony123@gmail.com>
2024-03-22 08:40:45 +0100

    added paths and router

--- frontend/eventify/src/index.js
+++ frontend/eventify/src/index.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css';
-import App from './App';
+// import App from './App';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+// import Root from './pages/Root';
+import HomePage from './pages/HomePage';
+import SearchPage from './pages/SearchPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />
+  },
+  {
+    path: '/register',
+    element: <RegisterPage />
+  },
+  {
+    path: '/events',
+    element: <SearchPage />
+  },
+  {
+    path: '/events/:eventId',
+    element: ''
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );

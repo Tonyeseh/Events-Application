e915717088ceefc38231dac9303df3dbd4a85b62
esehtony <esehtony123@gmail.com>
2024-03-25 05:02:39 +0100

    moved routing to App.js

--- frontend/eventify/src/index.js
+++ frontend/eventify/src/index.js
@@ -1,40 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-// import App from './App';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-// import Root from './pages/Root';
-import HomePage from './pages/HomePage';
-import SearchPage from './pages/SearchPage';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/login',
-    element: <LoginPage />
-  },
-  {
-    path: '/register',
-    element: <RegisterPage />
-  },
-  {
-    path: '/events',
-    element: <SearchPage />
-  },
-  {
-    path: '/events/:eventId',
-    element: ''
-  }
-])
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );

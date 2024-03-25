de654fe5943aac4cb8611461f68954e6f48547d1
esehtony <esehtony123@gmail.com>
2024-03-25 05:01:41 +0100

    routes for app aded

--- frontend/eventify/src/App.js
+++ frontend/eventify/src/App.js
@@ -1,25 +1,36 @@
 import React from "react";
-import Header from './components/Header';
-import Hero from './components/Hero'
-import CategorySection from "./components/CategorySection";
-import PopularInLocation from "./components/PopularInLocation";
-import BestOnlineSection from "./components/BestOnlineSection";
-import TrendingSection from "./components/TrendingSection";
-import Footer from "./components/Footer";
-import NewsLetterSection from "./components/NewLetterSection";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import HomePage from './pages/HomePage';
+import SearchPage from './pages/SearchPage';
+import EventPage from './pages/EventPage';
+import CreateEvent from './pages/CreateEvent';
+import EventBanner from "./pages/EventBanner";
+import TicketingPage from "./pages/TicketingPage";
+import ReviewEvent from "./pages/ReviewEvent";
+
+import { FormProvider } from "./context/FormContext";
 
 function App() {
   return (
-    <div>
-        <Header />
-        <Hero />
-        <CategorySection />
-        <PopularInLocation />
-        <BestOnlineSection />
-        <TrendingSection />
-        <NewsLetterSection />
-        <Footer />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={ <HomePage /> } />
+        <Route path="events" exact element={ <SearchPage /> } />
+        <Route path="/events/:id" element={ <EventPage /> } />
+        <Route path="/login" element={ <LoginPage /> } />
+        <Route path="/register" element={ <RegisterPage /> } />
+        </Routes>
+        <FormProvider>
+          <Routes>
+          <Route path="/create-event" element={ <CreateEvent /> } />
+          <Route path="/create-event/banner" element={ <EventBanner /> } />
+          <Route path="/create-event/ticketing" element={ <TicketingPage /> } />
+          <Route path="/create-event/review" element={ <ReviewEvent /> } />
+        </Routes>
+      </FormProvider>
+    </>
   );
 }
 

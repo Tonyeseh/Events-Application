799e06ea7f66aa870f025601f7fe42b7b1f9e954
esehtony <esehtony123@gmail.com>
2024-03-25 04:17:54 +0100

    form context for create event form

---
+++ frontend/eventify/src/hooks/UseFormContext.js
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import FormContext from "../context/FormContext";
+
+const UseFormContext = () => {
+  return (
+    useContext(FormContext)
+  )
+}
+
+export default UseFormContext;

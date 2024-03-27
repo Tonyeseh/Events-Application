881df324bcfa26213e9aebb30c677d0fb9759450
esehtony <esehtony123@gmail.com>
2024-03-27 15:09:23 +0100

    users route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
+import UsersController from "../controllers/UsersController";
+import verifyJWT from "../utils/verifyJWT";
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const usersRouter = Router()
 
-module.exports = router;
+usersRouter.get('/profile', verifyJWT, UsersController.getProfile)
+// usersRouter.put('/users/update-password', UsersController.updatePassword)
+// usersRouter.put('/users/update-email')
+// usersRouter.put('/users', UsersController.updateUser)
+// usersRouter.put('/users/reset-password', UsersController.rest)
+
+
+export default usersRouter;

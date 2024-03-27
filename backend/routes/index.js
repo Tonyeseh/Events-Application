507b218e94d2c6a26ffac0662a61b5a6766506ec
esehtony <esehtony123@gmail.com>
2024-03-27 15:11:15 +0100

    routes for index...

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+import { Router } from "express";
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const indexRouter = Router()
 
-module.exports = router;
+// indexRouter.get('/status', IndexController.getStatus)
+// indexRouter.get('/stats', IndexController.getStats)
+
+export default indexRouter

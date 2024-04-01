9cf486c72777f35392d3a3f134aee21ae1407f3c
esehtony <esehtony123@gmail.com>
2024-04-01 18:47:24 +0100

    added prettier formatting to Button

--- frontend/eventify/src/components/Button.jsx
+++ frontend/eventify/src/components/Button.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 const Button = ({ action, type }) => {
-    return (
-        <button className="w-full py-3 px-10 rounded-md bg-[#2B293D] text-white text-base font-bold" type={type}>
-            {action}
-        </button>
-    )
-}
+  return (
+    <button
+      className='w-full py-3 px-10 rounded-md bg-[#2B293D] text-white text-base font-bold'
+      type={type}
+    >
+      {action}
+    </button>
+  );
+};
 
 export default Button;

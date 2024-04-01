fc15fb9b850591c5b39c2f6f8eb636bc7a23bf48
esehtony <esehtony123@gmail.com>
2024-04-01 18:43:43 +0100

    added prettier formatting to ForYouSection

--- frontend/eventify/src/components/ForYouSection.jsx
+++ frontend/eventify/src/components/ForYouSection.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 
-
 const ForYouSection = () => {
-    return (
-        <div>
-            <img src="" alt="" />
-            <h1>
-            Events specially curated for you!
-            </h1>
-            <p>
-            Get event suggestions tailored to your interests! Don't let your favorite events slip away.
-            </p>
-            <button>Get Started</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <img src='' alt='' />
+      <h1>Events specially curated for you!</h1>
+      <p>
+        Get event suggestions tailored to your interests! Don't let your
+        favorite events slip away.
+      </p>
+      <button>Get Started</button>
+    </div>
+  );
+};
 
-export default ForYouSection
+export default ForYouSection;

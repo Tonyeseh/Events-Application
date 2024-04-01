b98a0259b2534d3b0faa6bcd9caf2973f4c6b203
esehtony <esehtony123@gmail.com>
2024-04-01 18:46:11 +0100

    added prettier formatting to Category

--- frontend/eventify/src/components/Category.jsx
+++ frontend/eventify/src/components/Category.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import catImg from '../img/cat_placeholder.png'
+import catImg from "../img/cat_placeholder.png";
 
-const Category = ({data}) => {
-    return (
-        <div className="w-32 h-32">
-            <img src={catImg} alt="" className="rounded-full h-28 w-28" />
-            <p className="text-center text-sm pt-7">{data.name}</p>
-        </div>
-    )
-}
+const Category = ({ data }) => {
+  return (
+    <div className='w-32 h-32'>
+      <img src={catImg} alt='' className='rounded-full h-28 w-28' />
+      <p className='text-center text-sm pt-7'>{data.name}</p>
+    </div>
+  );
+};
 
-export default Category
+export default Category;

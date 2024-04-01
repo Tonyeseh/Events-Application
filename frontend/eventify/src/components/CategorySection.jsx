42aa91cc6f9e356d927d1ccb4ff249042f247a50
esehtony <esehtony123@gmail.com>
2024-04-01 18:44:27 +0100

    added prettier formatting to CategorySection

--- frontend/eventify/src/components/CategorySection.jsx
+++ frontend/eventify/src/components/CategorySection.jsx
@@ -2,38 +2,48 @@ import React from "react";
 import Category from "./Category";
 
 const CategorySection = () => {
-    return (
-        <div className="mx-20 my-10">
-            <h1 className="font-bold text-xl pb-7">
-                Explore Categories
-            </h1>
-            <div className="flex flex-wrap gap-20 justify-between items-center">
-                <Category data={{
-                    name: 'Entertainment'
-                }} />
+  return (
+    <div className='mx-20 my-10'>
+      <h1 className='font-bold text-xl pb-7'>Explore Categories</h1>
+      <div className='flex flex-wrap gap-20 justify-between items-center'>
+        <Category
+          data={{
+            name: "Entertainment",
+          }}
+        />
 
-                <Category data={{
-                    name: 'Educational & Business'
-                }} />
+        <Category
+          data={{
+            name: "Educational & Business",
+          }}
+        />
 
-                <Category data={{
-                    name: 'Cultural & Arts'
-                }} />
+        <Category
+          data={{
+            name: "Cultural & Arts",
+          }}
+        />
 
-                <Category data={{
-                    name: 'Sports & Fitness'
-                }} />
+        <Category
+          data={{
+            name: "Sports & Fitness",
+          }}
+        />
 
-                <Category data={{
-                    name: 'Technology & Innovation'
-                }} />
+        <Category
+          data={{
+            name: "Technology & Innovation",
+          }}
+        />
 
-                <Category data={{
-                    name: 'Travel & Adventure'
-                }} />
-            </div>
-        </div>
-    )
-}
+        <Category
+          data={{
+            name: "Travel & Adventure",
+          }}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default CategorySection
+export default CategorySection;

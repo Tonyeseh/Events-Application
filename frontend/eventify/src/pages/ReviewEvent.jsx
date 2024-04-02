14ab4ec791d99422686f1516d999992ef49e08bb
esehtony <esehtony123@gmail.com>
2024-04-02 14:12:38 +0100

    dynamic content for reviewBox in ReviewEvent

--- frontend/eventify/src/pages/ReviewEvent.jsx
+++ frontend/eventify/src/pages/ReviewEvent.jsx
@@ -124,7 +124,9 @@ const ReviewEvent = () => {
                         d='M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z'
                       />
                     </svg>
-                    <span className='align-middle'>{data.session}</span>
+                    <span className='align-middle'>
+                      {data?.session[0]?.startDate}
+                    </span>
                   </p>
                   <p className='text-xs font-thin my-2'>
                     <svg
@@ -141,7 +143,10 @@ const ReviewEvent = () => {
                         d='M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'
                       />
                     </svg>
-                    <span className='align-middle'>3PM - 5PM</span>
+                    <span className='align-middle'>
+                      {data?.session[0]?.startTime} -{" "}
+                      {data?.session[0]?.endTime}
+                    </span>
                   </p>
                 </div>
                 <div className=''>
@@ -161,7 +166,7 @@ const ReviewEvent = () => {
                     Buy Tickets
                   </button>
                   <h3 className='font-bold my-3'>Ticket Information</h3>
-                  {data.tickets.map((element, idx) => (
+                  {/* {data.tickets.map((element, idx) => (
                     <p className='text-xs font-thin my-2' key={idx}>
                       <svg
                         xmlns='http://www.w3.org/2000/svg'
@@ -177,7 +182,7 @@ const ReviewEvent = () => {
                       </svg>
                       {element.ticketName} Ticket: ${element.ticketPrice} each
                     </p>
-                  ))}
+                  ))} */}
                 </div>
               </div>
 

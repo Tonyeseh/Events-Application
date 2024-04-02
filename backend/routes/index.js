c72f737df7d992d126893137a3ddd8836523c7da
esehtony <esehtony123@gmail.com>
2024-04-02 13:41:31 +0100

    added get image route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import IndexController from "../controllers/IndexController";
 
-const indexRouter = Router()
+const indexRouter = Router();
 
 // indexRouter.get('/status', IndexController.getStatus)
 // indexRouter.get('/stats', IndexController.getStats)
+indexRouter.get("/uploads/:fileName", IndexController.getImage);
 
-export default indexRouter
+export default indexRouter;

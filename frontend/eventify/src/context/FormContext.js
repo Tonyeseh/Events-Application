106b4e716a8ef640b45e482e7d7a910573203d7e
esehtony <esehtony123@gmail.com>
2024-04-02 13:58:08 +0100

    prettier formatted file FormContext.js

--- frontend/eventify/src/context/FormContext.js
+++ frontend/eventify/src/context/FormContext.js
@@ -1,62 +1,62 @@
 import { createContext, useState } from "react";
 
-const FormContext = createContext({})
+const FormContext = createContext({});
 
 export const FormProvider = ({ children }) => {
-    const title = {
-        0: 'Edit',
-        1: 'Banner',
-        2: 'Ticketing',
-        3: 'review',
+  const title = {
+    0: "Edit",
+    1: "Banner",
+    2: "Ticketing",
+    3: "review",
+  };
+  const [page, setPage] = useState(0);
+  const [data, setFormData] = useState({
+    title: "",
+    category: "",
+    type: "",
+    session: [],
+    location: "",
+    description: "",
+    address: "",
+    ticketType: "",
+    tickets: [],
+    isPublished: false,
+    tags: [],
+    coverImg: "",
+  });
+
+  const handleChange = (e) => {
+    const type = e.target.type;
+
+    const name = e.target.name;
+
+    const value = type === "file" ? e.target.files[0] : e.target.value;
+
+    if (name.startsWith("session")) {
+      setFormData((prevData) => {
+        prevData.session = {
+          ...prevData.session,
+          [name]: value,
+        };
+        return prevData;
+      });
+    } else {
+      setFormData((prevData) => {
+        return {
+          ...prevData,
+          [name]: value,
+        };
+      });
     }
-    const [page, setPage] = useState(0)
-    const [data, setFormData] = useState({
-        title: '',
-        category: '',
-        type: '',
-        session: [],
-        location: '',
-        description: '',
-        address: '',
-        ticket_type: '',
-        tickets: [],
-        isPublished: false,
-        tags: [],
-        cover_img: '',
-    })
+  };
 
-    const handleChange = e => {
-        const type = e.target.type
+  return (
+    <FormContext.Provider
+      value={{ title, page, setPage, data, setFormData, handleChange }}
+    >
+      {children}
+    </FormContext.Provider>
+  );
+};
 
-        const name = e.target.name
-
-        const value = type === 'file' ? e.target.files[0] : e.target.value
-
-        if (name.startsWith('session')) {
-
-            setFormData(prevData => {
-                prevData.session = {
-                    ...prevData.session,
-                    [name]: value
-                }
-                return prevData
-            })
-        } else {
-        setFormData(prevData => {
-            return {
-                ...prevData,
-                [name]: value,
-            }
-        })
-    }
-        console.log(data)
-    }
-
-    return (
-        <FormContext.Provider value={{title, page, setPage, data, setFormData, handleChange}}>
-            {children}
-        </FormContext.Provider>
-    )
-}
-
-export default FormContext
+export default FormContext;

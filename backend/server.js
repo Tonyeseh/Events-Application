f197d4f3452dc66f5d551cb4f0a7df6301bee8e2
esehtony <esehtony123@gmail.com>
2024-04-02 13:34:39 +0100

    prettier formatter on server.js

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,19 @@
-import express from 'express';
+import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import morgan from "morgan";
-import eventRouter from './routes/events';
-import indexRouter from './routes/index';
-import usersRouter from './routes/users';
-import authRouter from './routes/auth'
-import corsOptions from './config/corsOptions';
-import credentials from './utils/credentials';
+import eventRouter from "./routes/events";
+import indexRouter from "./routes/index";
+import usersRouter from "./routes/users";
+import authRouter from "./routes/auth";
+import corsOptions from "./config/corsOptions";
+import credentials from "./utils/credentials";
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
 app.use(credentials);
 
@@ -24,12 +24,12 @@ app.use(express.json());
 app.use(cookieParser());
 
 // routes
-app.use('/', indexRouter);
-app.use('/auth', authRouter)
-app.use('/users', usersRouter)
-app.use('/events', eventRouter)
+app.use("/", indexRouter);
+app.use("/auth", authRouter);
+app.use("/user", usersRouter);
+app.use("/events", eventRouter);
 // app.use('/categories', categoriesRouter)
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Listening on port ${PORT}`);
 });

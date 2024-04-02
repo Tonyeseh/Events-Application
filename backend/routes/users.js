c045a063b79a256c0e1717e60f700e519fb2fa37
esehtony <esehtony123@gmail.com>
2024-04-02 13:42:53 +0100

    added interested route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,13 +2,13 @@ import { Router } from "express";
 import UsersController from "../controllers/UsersController";
 import verifyJWT from "../utils/verifyJWT";
 
-const usersRouter = Router()
+const usersRouter = Router();
 
-usersRouter.get('/profile', verifyJWT, UsersController.getProfile)
+usersRouter.get("/profile", verifyJWT, UsersController.getProfile);
+usersRouter.get("/interested", verifyJWT, UsersController.getInterestedEvents);
 // usersRouter.put('/users/update-password', UsersController.updatePassword)
 // usersRouter.put('/users/update-email')
 // usersRouter.put('/users', UsersController.updateUser)
 // usersRouter.put('/users/reset-password', UsersController.rest)
-
 
 export default usersRouter;

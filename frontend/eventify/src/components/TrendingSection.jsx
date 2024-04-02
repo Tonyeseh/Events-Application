361be7969635635c17c6476df3f86c51fe310601
esehtony <esehtony123@gmail.com>
2024-04-02 14:24:10 +0100

    added dynamic content from parent TrendingSection
    also prettier formatted it

--- frontend/eventify/src/components/TrendingSection.jsx
+++ frontend/eventify/src/components/TrendingSection.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 import EventCard from "./Cards/EventCard";
 
-const TrendingSection = () => {
-    return (
-        <div className="mx-20 my-32">
-            <h1 className="font-bold text-xl pb-6">
-                Trending Events around the World
-            </h1>
-            <div className="flex flex-wrap -mx-5 -mb-10">
-                <EventCard />
-                <EventCard />
-                <EventCard />
-                <EventCard />
-                <EventCard />
-                <EventCard />
-            </div>
-            <div className="flex justify-center mt-10">
-                <button className="text-[#2B293D] border border-[#2B293D] px-32 py-2 rounded-lg text-center" type="button">See more</button>
-            </div>
-        </div>
-    )
-}
+const TrendingSection = ({ events }) => {
+  return (
+    <div className='mx-20 my-32'>
+      <h1 className='font-bold text-xl pb-6'>
+        Trending Events around the World
+      </h1>
+      <div className='flex flex-wrap -mx-5 -mb-10'>
+        {events &&
+          events.map((event) => <EventCard key={event._id} event={event} />)}
+      </div>
+      <div className='flex justify-center mt-10'>
+        <button
+          className='text-[#2B293D] border border-[#2B293D] px-32 py-2 rounded-lg text-center'
+          type='button'
+        >
+          See more
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default TrendingSection
+export default TrendingSection;

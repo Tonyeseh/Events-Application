b7068455cfa43ee1bd98b7b2675219e6cd8f8e0d
esehtony <esehtony123@gmail.com>
2024-04-02 13:29:25 +0100

    prettier formatter and added userInterested function

--- backend/controllers/UsersController.js
+++ backend/controllers/UsersController.js
@@ -1,16 +1,49 @@
-import dbClient from "../utils/db"
+import dbClient from "../utils/db";
 
 export default class UsersController {
-    static async getProfile (req, res) {
-        console.log(req.userEmail)
-        if (!req.userEmail) return res.status(403).json({ error: 'Forbidden' });
+  static async getProfile(req, res) {
+    console.log(req.userEmail);
+    if (!req.userEmail) return res.status(403).json({ error: "Forbidden" });
 
-        const user = await (await dbClient.usersCollection()).findOne({ email: req.userEmail });
+    const user = await (
+      await dbClient.usersCollection()
+    ).findOne({ email: req.userEmail });
 
-        if (!user) return res.status(403).json({ error: 'Forbidden' });
-        
-        delete user.password
+    if (!user) return res.status(403).json({ error: "Forbidden" });
 
-        return res.status(200).json(user)
+    delete user.password;
+
+    return res.status(200).json(user);
+  }
+
+  static async getInterestedEvents(req, res) {
+    const { userEmail } = req;
+
+    if (!userEmail) return res.status(403).json({ error: "Forbidden" });
+
+    try {
+      const user = await (
+        await dbClient.usersCollection()
+      ).findOne({ email: req.userEmail });
+
+      if (!user) return res.status(403).json({ error: "Forbidden" });
+
+      const eventIds = await (await dbClient.interestedCollection())
+        .find({ userId: user._id })
+        .toArray();
+      const events = await Promise.all(
+        eventIds.map(async (element) => {
+          const event = await (
+            await dbClient.eventsCollection()
+          ).findOne({ _id: element.eventId });
+
+          return event;
+        })
+      );
+      res.json({ events });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Something went wrong!" });
     }
+  }
 }

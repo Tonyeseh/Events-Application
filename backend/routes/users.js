b9fc38ba75dee16fe12ecc0bcb3ee56c86ca3ddf
Tonyeseh <esehtony123@gmail.com>
2024-04-18 14:22:58 +0100

    added /myevents route to user router

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,6 +6,7 @@ const usersRouter = Router();
 
 usersRouter.get("/profile", verifyJWT, UsersController.getProfile);
 usersRouter.get("/interested", verifyJWT, UsersController.getInterestedEvents);
+usersRouter.get("/myevents", verifyJWT, UsersController.getUserEvents);
 // usersRouter.put('/users/update-password', UsersController.updatePassword)
 // usersRouter.put('/users/update-email')
 // usersRouter.put('/users', UsersController.updateUser)

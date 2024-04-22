0f4bfdb211dbdc2ddfffbbed7ac3b243f1742b81
Tonyeseh <esehtony123@gmail.com>
2024-04-22 10:20:46 +0100

    added my event link to header popover

--- frontend/eventify/src/components/Header.jsx
+++ frontend/eventify/src/components/Header.jsx
@@ -165,6 +165,14 @@ const Header = () => {
                 >
                   Interests
                 </Link>
+                <li>
+                  <Link
+                    to="/myevents"
+                    className="flex items-center text-[13px] py-1.5 px-4 text-gray-600 hover:text-[#ffe047] hover:bg-gray-50"
+                  >
+                    My Events
+                  </Link>
+                </li>
               </li>
               <li>
                 <Link

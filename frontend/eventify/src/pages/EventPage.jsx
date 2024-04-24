9325669978602913ad6f317d89ad4e692dd13cae
Tonyeseh <esehtony123@gmail.com>
2024-04-24 12:25:29 +0100

    added events ticket information to the Event and Reveiw event pages

--- frontend/eventify/src/pages/EventPage.jsx
+++ frontend/eventify/src/pages/EventPage.jsx
@@ -90,7 +90,7 @@ const EventPage = () => {
           <>
             <div className="my-7 mx-7 md:mx-20 text-[#2B293D] border-b">
               <img
-                className="w-full h-20 md:h-96 rounded-xl"
+                className="w-full h-[50vh] md:h-[80vh] rounded-xl"
                 src={`http://localhost:5000/${eventData.coverImg}`}
                 alt=""
               />
@@ -133,7 +133,7 @@ const EventPage = () => {
               <div className="w-full flex flex-wrap justify-between">
                 <div className="md:w-1/2">
                   <h2 className="text-lg font-bold my-2">Date and Time</h2>
-                  <p className="text-xs font-thin my-2">
+                  <p className="font-thin my-2">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
@@ -160,7 +160,7 @@ const EventPage = () => {
                       )}
                     </span>
                   </p>
-                  <p className="text-xs font-thin my-2">
+                  <p className=" font-thin my-2">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
@@ -204,10 +204,27 @@ const EventPage = () => {
                     </svg>
                     Buy Tickets
                   </button>
-                  <h3 className="font-bold my-3">Ticket Information</h3>
-                  {eventData.tickets &&
+                  <h3 className="font-bold text-xl my-3">Ticket Information</h3>
+                  {eventData.ticketType === "free" ? (
+                    <p className="font-thin">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="w-4 h-4 mr-2 my-auto inline-block"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M1.5 6.375c0-1.036.84-1.875 1.875-1.875h17.25c1.035 0 1.875.84 1.875 1.875v3.026a.75.75 0 0 1-.375.65 2.249 2.249 0 0 0 0 3.898.75.75 0 0 1 .375.65v3.026c0 1.035-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 0 1 1.5 17.625v-3.026a.75.75 0 0 1 .374-.65 2.249 2.249 0 0 0 0-3.898.75.75 0 0 1-.374-.65V6.375Zm15-1.125a.75.75 0 0 1 .75.75v.75a.75.75 0 0 1-1.5 0V6a.75.75 0 0 1 .75-.75Zm.75 4.5a.75.75 0 0 0-1.5 0v.75a.75.75 0 0 0 1.5 0v-.75Zm-.75 3a.75.75 0 0 1 .75.75v.75a.75.75 0 0 1-1.5 0v-.75a.75.75 0 0 1 .75-.75Zm.75 4.5a.75.75 0 0 0-1.5 0V18a.75.75 0 0 0 1.5 0v-.75ZM6 12a.75.75 0 0 1 .75-.75H12a.75.75 0 0 1 0 1.5H6.75A.75.75 0 0 1 6 12Zm.75 2.25a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                      Free Ticket: $0 each
+                    </p>
+                  ) : (
+                    eventData.tickets &&
                     eventData.tickets.map((element, idx) => (
-                      <p className="text-xs font-thin" key={idx}>
+                      <p className=" font-thin" key={idx}>
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           viewBox="0 0 24 24"
@@ -222,11 +239,12 @@ const EventPage = () => {
                         </svg>
                         {element.ticketName} Ticket: ${element.ticketPrice} each
                       </p>
-                    ))}
+                    ))
+                  )}
                 </div>
                 <div className="w-full">
                   <h2 className="text-lg font-bold my-2">Location</h2>
-                  <p className="text-xs font-thin my-2">
+                  <p className=" font-thin my-2">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"

b253ff3d314ca79e0e841d9d96f927f5f154a956
Tonyeseh <esehtony123@gmail.com>
2024-04-25 21:25:34 +0100

    made inputs controlled and attributes react standard for Edit Profile Page

--- frontend/eventify/src/pages/EditProfile.jsx
+++ frontend/eventify/src/pages/EditProfile.jsx
@@ -1,12 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+// import { axiosPrivate } from "../api/axios";
+import useAuth from "../hooks/useAuth";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
 
 const EditProfile = () => {
+  const { auth } = useAuth();
+  const [userData, setUserData] = useState({});
+  const axiosPrivate = useAxiosPrivate();
+
+  const handleChange = (e) => {
+    const type = e.target.type;
+
+    const name = e.target.name;
+
+    const value = type === "file" ? e.target.files[0] : e.target.value;
+
+    setUserData((prevData) => {
+      return {
+        ...prevData,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axiosPrivate.post("user/profile", userData, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${auth.accessToken}`,
+        },
+      });
+
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axiosPrivate.get("/user/profile", {
+          headers: { Authorization: `Bearer ${auth.accessToken}` },
+        });
+        console.log(response);
+
+        setUserData(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUser();
+  }, [auth]);
+
   return (
     <>
       <Header />
-      {/* <form>
+      <div>
+        {/* <form>
         <div className="space-y-12">
           <div className="border-b border-gray-900/10 pb-12">
             <h2 className="text-base font-semibold leading-7 text-gray-900">
@@ -428,79 +482,84 @@ const EditProfile = () => {
           </button>
         </div>
       </form> */}
-      <section class="my-auto dark:bg-gray-900">
+      </div>
+      <section className="my-auto dark:bg-gray-900">
         <div className="my-10 mx-10 md:mx-24 text-[#2b293d]">
           <h1 className="my-7 text-2xl font-extrabold">Account Settings</h1>
-          <form>
-            <div class="w-full rounded-sm bg-[url('https://images.unsplash.com/photo-1449844908441-8829872d2607?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw2fHxob21lfGVufDB8MHx8fDE3MTA0MDE1NDZ8MA&ixlib=rb-4.0.3&q=80&w=1080')] bg-cover bg-center bg-no-repeat items-center">
-              <div class="mx-auto flex justify-center w-[141px] h-[141px] bg-blue-300/20 rounded-full bg-[url('https://images.unsplash.com/photo-1438761681033-6461ffad8d80?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw4fHxwcm9maWxlfGVufDB8MHx8fDE3MTEwMDM0MjN8MA&ixlib=rb-4.0.3&q=80&w=1080')] bg-cover bg-center bg-no-repeat">
-                <div class="bg-white/90 rounded-full w-6 h-6 text-center ml-28 mt-4">
+          <form onSubmit={handleSubmit}>
+            <div className="w-full rounded-sm bg-[url('https://images.unsplash.com/photo-1449844908441-8829872d2607?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw2fHxob21lfGVufDB8MHx8fDE3MTA0MDE1NDZ8MA&ixlib=rb-4.0.3&q=80&w=1080')] bg-cover bg-center bg-no-repeat items-center">
+              <div className="mx-auto flex justify-center w-[141px] h-[141px] bg-blue-300/20 rounded-full bg-[url('https://images.unsplash.com/photo-1438761681033-6461ffad8d80?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw4fHxwcm9maWxlfGVufDB8MHx8fDE3MTEwMDM0MjN8MA&ixlib=rb-4.0.3&q=80&w=1080')] bg-cover bg-center bg-no-repeat">
+                <div className="bg-white/90 rounded-full w-6 h-6 text-center ml-28 mt-4">
                   <input
                     type="file"
-                    name="profile"
-                    id="upload_profile"
+                    name="profilePics"
+                    id="profilePics"
+                    accept="image/*"
+                    onChange={handleChange}
                     hidden
-                    required
+                    // required
                   />
 
-                  <label for="upload_profile">
+                  <label htmlFor="profilePics">
                     <svg
                       data-slot="icon"
-                      class="w-6 h-5 text-blue-700"
+                      className="w-6 h-5 text-blue-700 cursor-pointer"
                       fill="none"
-                      stroke-width="1.5"
+                      strokeWidth="1.5"
                       stroke="currentColor"
                       viewBox="0 0 24 24"
                       xmlns="http://www.w3.org/2000/svg"
                       aria-hidden="true"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z"
                       ></path>
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z"
                       ></path>
                     </svg>
                   </label>
                 </div>
               </div>
-              <div class="flex justify-end">
+              <div className="flex justify-end">
                 <input
                   type="file"
-                  name="profile"
-                  id="upload_cover"
+                  name="profileCover"
+                  id="profileCover"
+                  accept="image/*"
+                  onChange={handleChange}
                   hidden
-                  required
+                  // required
                 />
 
-                <div class="bg-white flex items-center gap-1 rounded-tl-md px-2 text-center font-semibold">
+                <div className="bg-white flex items-center gap-1 rounded-tl-md px-2 text-center font-semibold cursor-pointer">
                   <label
-                    for="upload_cover"
-                    class="inline-flex items-center gap-1 cursor-pointer"
+                    htmlFor="profileCover"
+                    className="inline-flex items-center gap-1 cursor-pointer"
                   >
                     Cover
                     <svg
                       data-slot="icon"
-                      class="w-6 h-5 text-blue-700"
+                      className="w-6 h-5 text-blue-700"
                       fill="none"
-                      stroke-width="1.5"
+                      strokeWidth="1.5"
                       stroke="currentColor"
                       viewBox="0 0 24 24"
                       xmlns="http://www.w3.org/2000/svg"
                       aria-hidden="true"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z"
                       ></path>
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z"
                       ></path>
                     </svg>
@@ -508,7 +567,7 @@ const EditProfile = () => {
                 </div>
               </div>
             </div>
-            <h2 class="text-center mt-1 font-semibold dark:text-gray-300">
+            <h2 className="text-center mt-1 font-semibold dark:text-gray-300">
               Upload Profile and Cover Image
             </h2>
 
@@ -532,6 +591,8 @@ const EditProfile = () => {
                   id="firstName"
                   placeholder="Enter first name"
                   name="firstName"
+                  value={userData.firstName}
+                  onChange={handleChange}
                 />
               </div>
               <div className="flex justify-around my-3 gap-4">
@@ -547,13 +608,12 @@ const EditProfile = () => {
                   id="lastName"
                   placeholder="Enter last name"
                   name="lastName"
+                  value={userData.lastName}
+                  onChange={handleChange}
                 />
               </div>
               <div className="flex justify-around my-3 gap-4">
-                <label
-                  className="font-bold w-1/4 text-right"
-                  htmlFor="website"
-                >
+                <label className="font-bold w-1/4 text-right" htmlFor="website">
                   Website:
                 </label>
                 <input
@@ -562,13 +622,12 @@ const EditProfile = () => {
                   id="website"
                   placeholder="Enter website address"
                   name="website"
+                  value={userData.website}
+                  onChange={handleChange}
                 />
               </div>
               <div className="flex justify-around my-3 gap-4">
-                <label
-                  className="font-bold w-1/4 text-right"
-                  htmlFor="company"
-                >
+                <label className="font-bold w-1/4 text-right" htmlFor="company">
                   Company:
                 </label>
                 <input
@@ -577,6 +636,8 @@ const EditProfile = () => {
                   id="company"
                   placeholder="Enter company name"
                   name="company"
+                  value={userData.company}
+                  onChange={handleChange}
                 />
               </div>
             </div>
@@ -595,23 +656,22 @@ const EditProfile = () => {
               <div className="flex justify-around my-3 gap-4">
                 <label
                   className="font-bold w-1/4 text-right"
-                  htmlFor="phone"
+                  htmlFor="phoneNumber"
                 >
                   Phone Number:
                 </label>
                 <input
                   className="p-2 border border-[#8282827e] rounded-md mb-5 w-full focus:outline-none focus:ring focus:border-[#2b293d] placeholder:text-[#8282827e] placeholder:text-sm"
                   type="tel"
-                  id="phone"
+                  id="phoneNumber"
                   placeholder="Enter phone number"
-                  name="phone"
+                  name="phoneNumber"
+                  value={userData.phoneNumber}
+                  onChange={handleChange}
                 />
               </div>
               <div className="flex justify-around my-3 gap-4">
-                <label
-                  className="font-bold w-1/4 text-right"
-                  htmlFor="address"
-                >
+                <label className="font-bold w-1/4 text-right" htmlFor="address">
                   Address:
                 </label>
                 <textarea
@@ -620,13 +680,12 @@ const EditProfile = () => {
                   id="address"
                   placeholder="Enter address"
                   name="address"
+                  value={userData.address}
+                  onChange={handleChange}
                 />
               </div>
               <div className="flex justify-around my-3 gap-4">
-                <label
-                  className="font-bold w-1/4 text-right"
-                  htmlFor="city"
-                >
+                <label className="font-bold w-1/4 text-right" htmlFor="city">
                   City/Town:
                 </label>
                 <input
@@ -635,13 +694,12 @@ const EditProfile = () => {
                   id="city"
                   placeholder="Enter city"
                   name="city"
+                  value={userData.city}
+                  onChange={handleChange}
                 />
               </div>
               <div className="flex justify-around my-3 gap-4">
-                <label
-                  className="font-bold w-1/4 text-right"
-                  htmlFor="country"
-                >
+                <label className="font-bold w-1/4 text-right" htmlFor="country">
                   Country:
                 </label>
                 <input
@@ -650,6 +708,8 @@ const EditProfile = () => {
                   id="country"
                   placeholder="Enter country"
                   name="country"
+                  value={userData.country}
+                  onChange={handleChange}
                 />
               </div>
             </div>
@@ -657,8 +717,7 @@ const EditProfile = () => {
             <div className="flex justify-end">
               <button
                 className="bg-[#2b293d] text-white rounded-md py-2 px-5 font-bold"
-                type="button"
-                // onClick={validateForm}
+                type="submit"
               >
                 Save My Profile
               </button>

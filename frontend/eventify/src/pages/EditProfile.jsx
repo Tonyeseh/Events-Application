b8d86f42c0a4ffbbc380227c1a7b0bea678b167f
esehtony <esehtony123@gmail.com>
2024-04-25 14:02:56 +0100

    added personal and contact parts of form

--- frontend/eventify/src/pages/EditProfile.jsx
+++ frontend/eventify/src/pages/EditProfile.jsx
@@ -429,145 +429,241 @@ const EditProfile = () => {
         </div>
       </form> */}
       <section class="my-auto dark:bg-gray-900">
-        <div class="lg:w-[80%] md:w-[90%] xs:w-[96%] mx-auto flex gap-2">
-          <div class="lg:w-[88%] md:w-[80%] sm:w-[88%] xs:w-full mx-auto shadow-2xl p-2 rounded-xl h-fit self-center dark:bg-gray-800/40">
-            <div class="">
-              <h1 class="lg:text-3xl md:text-2xl sm:text-xl xs:text-xl font-serif font-extrabold mb-2 dark:text-white">
-                Profile
-              </h1>
-              <h2 class="text-grey text-sm mb-4 dark:text-gray-400">
-                Create Profile
-              </h2>
-              <form>
-                <div class="w-full rounded-sm bg-[url('https://images.unsplash.com/photo-1449844908441-8829872d2607?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw2fHxob21lfGVufDB8MHx8fDE3MTA0MDE1NDZ8MA&ixlib=rb-4.0.3&q=80&w=1080')] bg-cover bg-center bg-no-repeat items-center">
-                  <div class="mx-auto flex justify-center w-[141px] h-[141px] bg-blue-300/20 rounded-full bg-[url('https://images.unsplash.com/photo-1438761681033-6461ffad8d80?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw4fHxwcm9maWxlfGVufDB8MHx8fDE3MTEwMDM0MjN8MA&ixlib=rb-4.0.3&q=80&w=1080')] bg-cover bg-center bg-no-repeat">
-                    <div class="bg-white/90 rounded-full w-6 h-6 text-center ml-28 mt-4">
-                      <input
-                        type="file"
-                        name="profile"
-                        id="upload_profile"
-                        hidden
-                        required
-                      />
+        <div className="my-10 mx-10 md:mx-24 text-[#2b293d]">
+          <h1 className="my-7 text-2xl font-extrabold">Account Settings</h1>
+          <form>
+            <div class="w-full rounded-sm bg-[url('https://images.unsplash.com/photo-1449844908441-8829872d2607?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw2fHxob21lfGVufDB8MHx8fDE3MTA0MDE1NDZ8MA&ixlib=rb-4.0.3&q=80&w=1080')] bg-cover bg-center bg-no-repeat items-center">
+              <div class="mx-auto flex justify-center w-[141px] h-[141px] bg-blue-300/20 rounded-full bg-[url('https://images.unsplash.com/photo-1438761681033-6461ffad8d80?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw4fHxwcm9maWxlfGVufDB8MHx8fDE3MTEwMDM0MjN8MA&ixlib=rb-4.0.3&q=80&w=1080')] bg-cover bg-center bg-no-repeat">
+                <div class="bg-white/90 rounded-full w-6 h-6 text-center ml-28 mt-4">
+                  <input
+                    type="file"
+                    name="profile"
+                    id="upload_profile"
+                    hidden
+                    required
+                  />
 
-                      <label for="upload_profile">
-                        <svg
-                          data-slot="icon"
-                          class="w-6 h-5 text-blue-700"
-                          fill="none"
-                          stroke-width="1.5"
-                          stroke="currentColor"
-                          viewBox="0 0 24 24"
-                          xmlns="http://www.w3.org/2000/svg"
-                          aria-hidden="true"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z"
-                          ></path>
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z"
-                          ></path>
-                        </svg>
-                      </label>
-                    </div>
-                  </div>
-                  <div class="flex justify-end">
-                    <input
-                      type="file"
-                      name="profile"
-                      id="upload_cover"
-                      hidden
-                      required
-                    />
+                  <label for="upload_profile">
+                    <svg
+                      data-slot="icon"
+                      class="w-6 h-5 text-blue-700"
+                      fill="none"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z"
+                      ></path>
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z"
+                      ></path>
+                    </svg>
+                  </label>
+                </div>
+              </div>
+              <div class="flex justify-end">
+                <input
+                  type="file"
+                  name="profile"
+                  id="upload_cover"
+                  hidden
+                  required
+                />
 
-                    <div class="bg-white flex items-center gap-1 rounded-tl-md px-2 text-center font-semibold">
-                      <label
-                        for="upload_cover"
-                        class="inline-flex items-center gap-1 cursor-pointer"
-                      >
-                        Cover
-                        <svg
-                          data-slot="icon"
-                          class="w-6 h-5 text-blue-700"
-                          fill="none"
-                          stroke-width="1.5"
-                          stroke="currentColor"
-                          viewBox="0 0 24 24"
-                          xmlns="http://www.w3.org/2000/svg"
-                          aria-hidden="true"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z"
-                          ></path>
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z"
-                          ></path>
-                        </svg>
-                      </label>
-                    </div>
-                  </div>
+                <div class="bg-white flex items-center gap-1 rounded-tl-md px-2 text-center font-semibold">
+                  <label
+                    for="upload_cover"
+                    class="inline-flex items-center gap-1 cursor-pointer"
+                  >
+                    Cover
+                    <svg
+                      data-slot="icon"
+                      class="w-6 h-5 text-blue-700"
+                      fill="none"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z"
+                      ></path>
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z"
+                      ></path>
+                    </svg>
+                  </label>
                 </div>
-                <h2 class="text-center mt-1 font-semibold dark:text-gray-300">
-                  Upload Profile and Cover Image
-                </h2>
-                <div class="flex lg:flex-row md:flex-col sm:flex-col xs:flex-col gap-2 justify-center w-full">
-                  <div class="w-full  mb-4 mt-6">
-                    <label for="" class="mb-2 dark:text-gray-300">
-                      First Name
-                    </label>
-                    <input
-                      type="text"
-                      class="mt-2 p-2 w-full border-2 rounded-lg dark:text-gray-200 dark:border-gray-600 dark:bg-gray-800"
-                      placeholder="First Name"
-                    />
-                  </div>
-                  <div class="w-full  mb-4 lg:mt-6">
-                    <label for="" class=" dark:text-gray-300">
-                      Last Name
-                    </label>
-                    <input
-                      type="text"
-                      class="mt-2 p-2 w-full border-2 rounded-lg dark:text-gray-200 dark:border-gray-600 dark:bg-gray-800"
-                      placeholder="Last Name"
-                    />
-                  </div>
-                </div>
-
-                <div class="flex lg:flex-row md:flex-col sm:flex-col xs:flex-col gap-2 justify-center w-full">
-                  <div class="w-full">
-                    <h3 class="dark:text-gray-300 mb-2">Sex</h3>
-                    <select class="w-full text-grey border-2 rounded-lg p-2 pl-2 pr-2 dark:text-gray-200 dark:border-gray-600 dark:bg-gray-800">
-                      <option disabled value="">
-                        Select Sex
-                      </option>
-                      <option value="Male">Male</option>
-                      <option value="Female">Female</option>
-                    </select>
-                  </div>
-                  <div class="w-full">
-                    <h3 class="dark:text-gray-300 mb-2">Date Of Birth</h3>
-                    <input
-                      type="date"
-                      class="text-grey p-2 w-full border-2 rounded-lg dark:text-gray-200 dark:border-gray-600 dark:bg-gray-800"
-                    />
-                  </div>
-                </div>
-                <div class="w-full rounded-lg bg-blue-500 mt-4 text-white text-lg font-semibold">
-                  <button type="submit" class="w-full p-2">
-                    Submit
-                  </button>
-                </div>
-              </form>
+              </div>
             </div>
-          </div>
+            <h2 class="text-center mt-1 font-semibold dark:text-gray-300">
+              Upload Profile and Cover Image
+            </h2>
+
+            <div className="my-5">
+              <div className="flex gap-4">
+                <div className="w-1/4"></div>
+                <h3 className="text-xl font-bold w-full">
+                  Personal Information
+                </h3>
+              </div>
+              <div className="flex justify-around my-3 gap-4">
+                <label
+                  className="font-bold w-1/4 text-right"
+                  htmlFor="firstName"
+                >
+                  First Name:
+                </label>
+                <input
+                  className="p-2 border border-[#8282827e] rounded-md mb-5 w-full focus:outline-none focus:ring focus:border-[#2b293d] placeholder:text-[#8282827e] placeholder:text-sm"
+                  type="text"
+                  id="firstName"
+                  placeholder="Enter first name"
+                  name="firstName"
+                />
+              </div>
+              <div className="flex justify-around my-3 gap-4">
+                <label
+                  className="font-bold w-1/4 text-right"
+                  htmlFor="lastName"
+                >
+                  Last Name:
+                </label>
+                <input
+                  className="p-2 border border-[#8282827e] rounded-md mb-5 w-full focus:outline-none focus:ring focus:border-[#2b293d] placeholder:text-[#8282827e] placeholder:text-sm"
+                  type="text"
+                  id="lastName"
+                  placeholder="Enter last name"
+                  name="lastName"
+                />
+              </div>
+              <div className="flex justify-around my-3 gap-4">
+                <label
+                  className="font-bold w-1/4 text-right"
+                  htmlFor="website"
+                >
+                  Website:
+                </label>
+                <input
+                  className="p-2 border border-[#8282827e] rounded-md mb-5 w-full focus:outline-none focus:ring focus:border-[#2b293d] placeholder:text-[#8282827e] placeholder:text-sm"
+                  type="text"
+                  id="website"
+                  placeholder="Enter website address"
+                  name="website"
+                />
+              </div>
+              <div className="flex justify-around my-3 gap-4">
+                <label
+                  className="font-bold w-1/4 text-right"
+                  htmlFor="company"
+                >
+                  Company:
+                </label>
+                <input
+                  className="p-2 border border-[#8282827e] rounded-md mb-5 w-full focus:outline-none focus:ring focus:border-[#2b293d] placeholder:text-[#8282827e] placeholder:text-sm"
+                  type="text"
+                  id="company"
+                  placeholder="Enter company name"
+                  name="company"
+                />
+              </div>
+            </div>
+            <div className="my-5">
+              <div className="flex gap-4">
+                <div className="w-1/4"></div>
+                <h3 className="text-xl font-bold w-full">Contact Details</h3>
+              </div>
+              <div className="flex gap-4">
+                <div className="w-1/4"></div>
+                <h3 className="font-thin w-full">
+                  These details are private and only used to contact you for
+                  ticketing or prizes.
+                </h3>
+              </div>
+              <div className="flex justify-around my-3 gap-4">
+                <label
+                  className="font-bold w-1/4 text-right"
+                  htmlFor="phone"
+                >
+                  Phone Number:
+                </label>
+                <input
+                  className="p-2 border border-[#8282827e] rounded-md mb-5 w-full focus:outline-none focus:ring focus:border-[#2b293d] placeholder:text-[#8282827e] placeholder:text-sm"
+                  type="tel"
+                  id="phone"
+                  placeholder="Enter phone number"
+                  name="phone"
+                />
+              </div>
+              <div className="flex justify-around my-3 gap-4">
+                <label
+                  className="font-bold w-1/4 text-right"
+                  htmlFor="address"
+                >
+                  Address:
+                </label>
+                <textarea
+                  className="p-2 border border-[#8282827e] rounded-md mb-5 w-full focus:outline-none focus:ring focus:border-[#2b293d] placeholder:text-[#8282827e] placeholder:text-sm"
+                  rows="3"
+                  id="address"
+                  placeholder="Enter address"
+                  name="address"
+                />
+              </div>
+              <div className="flex justify-around my-3 gap-4">
+                <label
+                  className="font-bold w-1/4 text-right"
+                  htmlFor="city"
+                >
+                  City/Town:
+                </label>
+                <input
+                  className="p-2 border border-[#8282827e] rounded-md mb-5 w-full focus:outline-none focus:ring focus:border-[#2b293d] placeholder:text-[#8282827e] placeholder:text-sm"
+                  type="text"
+                  id="city"
+                  placeholder="Enter city"
+                  name="city"
+                />
+              </div>
+              <div className="flex justify-around my-3 gap-4">
+                <label
+                  className="font-bold w-1/4 text-right"
+                  htmlFor="country"
+                >
+                  Country:
+                </label>
+                <input
+                  className="p-2 border border-[#8282827e] rounded-md mb-5 w-full focus:outline-none focus:ring focus:border-[#2b293d] placeholder:text-[#8282827e] placeholder:text-sm"
+                  type="text"
+                  id="country"
+                  placeholder="Enter country"
+                  name="country"
+                />
+              </div>
+            </div>
+
+            <div className="flex justify-end">
+              <button
+                className="bg-[#2b293d] text-white rounded-md py-2 px-5 font-bold"
+                type="button"
+                // onClick={validateForm}
+              >
+                Save My Profile
+              </button>
+            </div>
+          </form>
         </div>
       </section>
       <Footer />

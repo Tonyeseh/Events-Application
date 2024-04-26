ae93d3fafe5ed17c0a4b524bb36508c0b2d87225
Tonyeseh <esehtony123@gmail.com>
2024-04-26 07:09:45 +0100

    changed the routing of the pages

--- frontend/eventify/src/App.js
+++ frontend/eventify/src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
@@ -9,87 +14,80 @@ import CreateEvent from "./pages/CreateEvent";
 import EventBanner from "./pages/EventBanner";
 import TicketingPage from "./pages/TicketingPage";
 import ReviewEvent from "./pages/ReviewEvent";
-import { FormProvider } from "./context/FormContext";
 import RequireAuth from "./components/RequireAuth";
 import PersistentLogin from "./components/PersistLogin";
 import InterestedPage from "./pages/InterestedPage";
 import UserEventPage from "./pages/UserEvents";
 import EditProfile from "./pages/EditProfile";
 import ErrorPage from "./pages/ErrorPage";
+import CreateEventLayout from "./components/CreateEventLayout";
 
-function App() {
-  return (
-    <>
-      <Routes>
-        <Route element={<PersistentLogin />}>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<PersistentLogin />}>
+      <Route
+        path="/"
+        element={<HomePage />}
+      />
+      <Route
+        path="events"
+        exact
+        element={<SearchPage />}
+      />
+      <Route
+        path="/events/:eventId"
+        element={<EventPage />}
+      />
+      <Route
+        path="/login"
+        element={<LoginPage />}
+      />
+      <Route
+        path="/register"
+        element={<RegisterPage />}
+      />
+      <Route element={<RequireAuth />}>
+        <Route
+          path="/interested-events"
+          element={<InterestedPage />}
+        />
+        <Route element={<CreateEventLayout />}>
           <Route
-            path="/"
-            element={<HomePage />}
+            path="/create-event"
+            element={<CreateEvent />}
           />
           <Route
-            path="events"
-            exact
-            element={<SearchPage />}
+            path="/create-event/banner"
+            element={<EventBanner />}
           />
           <Route
-            path="/events/:eventId"
-            element={<EventPage />}
+            path="/create-event/ticketing"
+            element={<TicketingPage />}
           />
           <Route
-            path="/login"
-            element={<LoginPage />}
-          />
-          <Route
-            path="/register"
-            element={<RegisterPage />}
+            path="/create-event/review"
+            element={<ReviewEvent />}
           />
         </Route>
-      </Routes>
-      <FormProvider>
-        <Routes>
-          <Route
-            element={<PersistentLogin />}
-            errorElement={<ErrorPage />}
-          >
-            <Route element={<RequireAuth />}>
-              <Route
-                path="/interested-events"
-                element={<InterestedPage />}
-              />
-              <Route
-                path="/create-event"
-                element={<CreateEvent />}
-              />
-              <Route
-                path="/create-event/banner"
-                element={<EventBanner />}
-              />
-              <Route
-                path="/create-event/ticketing"
-                element={<TicketingPage />}
-              />
-              <Route
-                path="/create-event/review"
-                element={<ReviewEvent />}
-              />
-              <Route
-                path="/myevents"
-                element={<UserEventPage />}
-              />
-              <Route
-                path="/profile"
-                element={<EditProfile />}
-              />
-              <Route
-                path="*"
-                element={<ErrorPage />}
-              />
-            </Route>
-          </Route>
-        </Routes>
-      </FormProvider>
-    </>
-  );
+        <Route
+          path="/myevents"
+          element={<UserEventPage />}
+        />
+        <Route
+          path="/profile"
+          element={<EditProfile />}
+        />
+        <Route
+          path="*"
+          element={<ErrorPage />}
+        />
+      </Route>
+    </Route>
+  )
+);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- frontend/eventify/src/index.js
+++ frontend/eventify/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
 import App from "./App";
@@ -8,10 +7,8 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );

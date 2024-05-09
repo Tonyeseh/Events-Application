af22236fab93d099964469a0975dfabb440c28f4
Tonyeseh <esehtony123@gmail.com>
2024-05-09 22:39:31 +0100

    updated config for tailwind and next
    next config has to do with images and tailwind was flowbite

--- next_implementation/tailwind.config.ts
+++ next_implementation/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const flowbite = require("flowbite-react/tailwind");
 
 const config: Config = {
   content: [
@@ -6,6 +7,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -13,10 +15,12 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "home-hero":
+          "url(https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/coast-house-view.jpg)",
       },
     },
   },
   darkMode: "media",
-  plugins: [require("flowbite/plugin")],
+  plugins: [require("flowbite/plugin"), flowbite.plugin()],
 };
 export default config;

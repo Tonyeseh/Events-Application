7ff4099b95d324e37684a9ffb3fbbd4f0a0a3e35
Tonyeseh <esehtony123@gmail.com>
2024-05-09 22:37:05 +0100

    definitions for some data

---
+++ next_implementation/src/app/lib/definitions.ts
@@ -0,0 +1,30 @@
+export type EventObj = {
+  _id: string;
+  coverImg?: string;
+  category: string;
+  title: string;
+  ticketType: string;
+  interestCount?: number;
+  tickets?: ticket[] | null;
+  interested?: boolean;
+  location: string;
+  address: string;
+  session: aSession[];
+  type: string;
+  description: string;
+  isPublished: boolean;
+  tags?: null | string[];
+  userId: string;
+};
+
+export type ticket = {
+  ticketPrice: number | string;
+  ticketName: string;
+};
+
+export type aSession = {
+  startTime: string;
+  endTime: string;
+  startDate: string;
+  endDate?: string;
+};

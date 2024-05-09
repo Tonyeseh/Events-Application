e0840daa61bd39e617fa9c26b4e51950412ccfe7
Tonyeseh <esehtony123@gmail.com>
2024-05-09 22:38:39 +0100

    updated home page with new sections

--- next_implementation/src/app/page.tsx
+++ next_implementation/src/app/page.tsx
@@ -1,7 +1,17 @@
-import Image from "next/image";
+import BestOnlineSection from "./ui/home/bestOnlineSection";
+import Hero from "./ui/home/hero";
+import PopularInLocation from "./ui/home/popularInLocation";
+import TrendingSection from "./ui/home/trendingSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <>
+      <Hero />
+      {/* <main className="flex min-h-screen flex-col items-center justify-between p-24"> */}
+      <PopularInLocation />
+      <BestOnlineSection />
+      <TrendingSection />
+      {/* </main> */}
+    </>
   );
 }
